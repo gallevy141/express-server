f9e5002117997f641776b08d64041350c4c87814
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-08-16 15:18:36 +0100

    specific order can be retrieved by id

--- myServer/routes/orders.js
+++ myServer/routes/orders.js
@@ -10,4 +10,12 @@ router.get('/', function(req, res) {
     res.json({ orders })
 })
 
+router.get('/:id', function(req, res) {
+    const order = orders.find(o => o.id === parseInt(req.params.id, 10))
+    if (!order) {
+        return res.status(404).json({ error: 'Order not found.' })
+    }
+    res.json({ order })
+})
+
 module.exports = router
