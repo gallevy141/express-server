f0c55ea800b2d09de8b24469680f6cd35a281992
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-08-16 14:56:16 +0100

    adding ability to list users with an optional age filter

--- myServer/routes/users.js
+++ myServer/routes/users.js
@@ -10,4 +10,13 @@ router.get('/details', function(req, res, next) {
   res.send('User details endpoint')
 })
 
+router.get('/', function(req, res) {
+  const { age } = req.query
+  if (age) {
+      const filteredUsers = users.filter(user => user.age === parseInt(age, 10))
+      return res.json({ users: filteredUsers })
+  }
+  res.json({ users })
+})
+
 module.exports = router
