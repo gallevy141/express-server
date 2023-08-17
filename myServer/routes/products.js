97bc17993391b09af9489274fabe49241e9ec026
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-08-17 15:23:39 +0100

    Adding product details

--- myServer/routes/products.js
+++ myServer/routes/products.js
@@ -7,3 +7,12 @@ const products = []
 router.get('/', (req, res, next) => {
   res.json({ products })
 })
+
+router.get('/:productid', (req, res, next) => {
+    const product = products.find(p => p.productId === req.params.productid)
+    if (product) {
+      res.json(product)
+    } else {
+      res.status(404).json({ message: 'Product not found.' })
+    }
+  })
