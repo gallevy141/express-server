7daaa40d7c2b569d21d58515be4b378ad2da3b36
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-08-17 15:07:37 +0100

    create app.js

--- myServer/app.js
+++ myServer/app.js
@@ -1,22 +1,3 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var usersRouter = require('./routes/users')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var ordersRouter = require('./routes/orders')
-
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/orders', ordersRouter)
-
-module.exports = app;
+app.use('/users', usersRouter)
