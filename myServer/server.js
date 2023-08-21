998cb568b5ca12b414161275ea2601cd6d033f79
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-08-21 15:44:57 +0100

    adding api routes

--- myServer/server.js
+++ myServer/server.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const productsRouter = require('./routes/products')
+const usersRouter = require('./routes/users')
+const ordersRouter = require('./routes/orders')
 
 const app = express()
 const PORT = 5000
 
+app.use(cors())
+app.use(bodyParser.json())
 app.use('/api/products', productsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/orders', ordersRouter)
 
 app.listen(PORT, () => {
-  console.log(`Server started on http://localhost:${PORT}`)
+    console.log(`Server started on http://localhost:${PORT}`)
 })
