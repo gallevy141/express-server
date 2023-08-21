3d45d6400355e9e6703cffbaf2cdf2eecbca074c
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-08-21 15:35:44 +0100

    adding encryption to user password

--- myServer/routes/users.js
+++ myServer/routes/users.js
@@ -1,43 +1,56 @@
 var express = require('express')
 var router = express.Router()
+const pool = require('../dal')
+const bcrypt = require('bcrypt')
 
-// Simulated database
-const users = []
+router.post('/register', async (req, res, next) => {
+    const { username, password, email } = req.body
 
-router.post('/register', (req, res, next) => {
-  const { username, password, email } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10)
 
-  const userId = new Date().toISOString() 
-
-  users.push({ userId, username, password, email })
-
-  res.json({ userId, message: 'User registered successfully.' })
+    try {
+        await pool.query('INSERT INTO User (name, email, password) VALUES (?, ?, ?)', [username, email, hashedPassword])
+        res.json({ message: 'User registered successfully.' })
+    } catch (error) {
+        res.status(500).json({ error: 'Error registering user' })
+    }
 })
 
-router.post('/login', (req, res, next) => {
-  const { username, password } = req.body;
+router.post('/login', async (req, res, next) => {
+    const { username, password } = req.body;
 
-  const user = users.find(u => u.username === username && u.password === password);
-  
-  if (user) {
-    res.json({ userId: user.userId, token: 'simulated_token', message: 'Login successful.' });
-  } else {
-    res.status(400).json({ message: 'Invalid credentials.' });
-  }
-});
+    try {
+        const [users] = await pool.query('SELECT * FROM User WHERE name = ?', [username])
+        const user = users[0]
 
-router.get('/:userId', (req, res, next) => {
-  const user = users.find(u => u.userId === req.params.userId)
-  if (user) {
-    res.json({ 
-      userId: user.userId, 
-      username: user.username, 
-      email: user.email, 
-      password: user.password,
-    })
-  } else {
-    res.status(404).json({ message: 'User not found.' })
-  }
+        if (user && await bcrypt.compare(password, user.password)) {
+            res.json({ userId: user.userID, token: 'simulated_token', message: 'Login successful.' })
+        } else {
+            res.status(400).json({ message: 'Invalid credentials.' })
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Error logging in' })
+    }
+})
+
+router.get('/:userId', async (req, res, next) => {
+    try {
+        const [users] = await pool.query('SELECT * FROM User WHERE userID = ?', [req.params.userId])
+        const user = users[0]
+
+        if (user) {
+            res.json({ 
+                userId: user.userID, 
+                username: user.name, 
+                email: user.email
+                // Exclude password for security
+            })
+        } else {
+            res.status(404).json({ message: 'User not found.' })
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching user' })
+    }
 })
 
 module.exports = router
