97b96e9e8a89f5d2a85614b8b8ee8d738d9c22d8
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-08-21 15:39:11 +0100

    changing orders to interact iwth server. change location of the file

---
+++ myServer/orders.js
@@ -0,0 +1,28 @@
+var express = require('express')
+var router = express.Router()
+const pool = require('../dal')
+
+router.get('/', async function(req, res) {
+    try {
+        const [orders] = await pool.query('SELECT * FROM Orders')
+        res.json(orders)
+    } catch (error) {
+        res.status(500).json({ error: 'Server error' })
+    }
+})
+
+router.post('/', async function(req, res) {
+    const { userId, product, amount } = req.body
+
+    if (!userId || !product || !amount) {
+        return res.status(400).json({ error: 'All fields are required.' })
+    }
+    try {
+        const result = await pool.query('INSERT INTO Orders (userId, product, amount) VALUES (?, ?, ?)', [userId, product, amount])
+        res.status(201).json({ message: 'Order created successfully.', orderID: result.insertId })
+    } catch (error) {
+        res.status(500).json({ error: 'Error creating order' })
+    }
+})
+
+module.exports = router
