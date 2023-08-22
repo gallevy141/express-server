f74c34b0ad6bd3374eac4f384706119b7ebcce04
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-08-22 12:33:18 +0100

    changing dal file to adjust for env

--- myServer/routes/dal.js
+++ myServer/routes/dal.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2/promise')
+require('dotenv').config()
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '83463901Gl',
-  database: 'ufc_store'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 })
 
 module.exports = pool
