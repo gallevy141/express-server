fb423daf710a91b1dd33640fd2b2de8e286925a0
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-08-23 12:51:34 +0100

    commenting out orders references

--- myServer/server.js
+++ myServer/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const productsRouter = require('./routes/products')
 const usersRouter = require('./routes/users')
-const ordersRouter = require('./routes/orders')
+//const ordersRouter = require('./routes/orders')
 
 const app = express()
 const PORT = 5000
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(cors())
 app.use('/api/products', productsRouter)
 app.use('/api/users', usersRouter)
-app.use('/api/orders', ordersRouter)
+//app.use('/api/orders', ordersRouter)
 
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`)
