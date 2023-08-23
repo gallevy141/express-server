ffb33a3cf873000a714e8bdc79c46b2afba01a66
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-08-23 12:47:28 +0100

    removing app.use bodyparser

--- myServer/server.js
+++ myServer/server.js
@@ -9,7 +9,6 @@ const PORT = 5000
 
 app.use(express.json())
 app.use(cors())
-app.use(bodyParser.json())
 app.use('/api/products', productsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/orders', ordersRouter)
