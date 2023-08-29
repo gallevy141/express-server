5dd622d8df788cbc7fbc425f91248f660147c559
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-08-29 19:00:13 +0100

    update GET with a limit parameter for the amount of products shown on the page

--- myServer/routes/products.js
+++ myServer/routes/products.js
@@ -4,7 +4,14 @@ const pool = require('./dal')
 
 router.get('/', async (req, res) => {
   try {
-    const [products] = await pool.query('SELECT * FROM Products')
+    const limit = req.query.limit
+    let query = 'SELECT * FROM Products'
+    
+    if (limit) {
+      query += ' ORDER BY productID DESC LIMIT ?'
+    }
+
+    const [products] = await pool.query(query, [parseInt(limit)])
     res.json(products)
   } catch (error) {
     res.status(500).json({ error: 'Server error' })
