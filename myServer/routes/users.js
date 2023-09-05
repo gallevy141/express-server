8cbf8c5102e84c95f25cc6b8b4f60582c7fa8a16
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-09-05 14:59:57 +0100

    adding verifications like existing email and that fields are not empty

--- myServer/routes/users.js
+++ myServer/routes/users.js
@@ -1,15 +1,24 @@
 const express = require('express')
-const router = express.Router()
-const pool = require('./dal')
+const router = express.Router();
+const pool = require('./dal');
 const bcrypt = require('bcrypt')
 
 router.post('/register', async (req, res, next) => {
     const { name, password, email } = req.body
 
+    if (!name || !password || !email) {
+        return res.status(400).json({ error: 'All fields are required!' })
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10)
 
+    const [existingUsers] = await pool.query('SELECT * FROM User WHERE email = ?', [email])
+    if (existingUsers.length) {
+        return res.status(400).json({ error: 'User with this email already exists!' })
+    }
+
     try {
-      await pool.query('INSERT INTO User (name, email, password) VALUES (?, ?, ?)', [name, email, hashedPassword])
+        await pool.query('INSERT INTO User (name, email, password) VALUES (?, ?, ?)', [name, email, hashedPassword])
         res.json({ message: 'User registered successfully.' })
     } catch (error) {
         res.status(500).json({ error: 'Error registering user' })
@@ -20,8 +29,8 @@ router.post('/login', async (req, res, next) => {
     const { name, password } = req.body;
 
     try {
-      const [users] = await pool.query('SELECT * FROM User WHERE name = ?', [name])
-        const user = users[0]
+        const [users] = await pool.query('SELECT * FROM User WHERE name = ?', [name])
+        const user = users[0];
 
         if (user && await bcrypt.compare(password, user.password)) {
             res.json({ userId: user.userID, token: 'simulated_token', message: 'Login successful.' })
@@ -43,7 +52,7 @@ router.get('/:userId', async (req, res, next) => {
                 userId: user.userID, 
                 username: user.name, 
                 email: user.email
-                // Exclude password for security
+                // Exclude password for security reasons
             })
         } else {
             res.status(404).json({ message: 'User not found.' })
