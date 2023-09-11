46c9adafe10628677eb75f3631f85634886a67ff
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-09-11 10:20:34 +0100

    averifies decrypted userId matched session request by user

--- myServer/routes/authMiddleware.js
+++ myServer/routes/authMiddleware.js
@@ -1,14 +1,21 @@
 const { decrypt } = require('../cryptoUtil')
 
 module.exports = (req, res, next) => {
-   const token = req.cookies.userData
-   if (!token) return res.status(401).send('Not authenticated')
+    const encryptedUserData = req.cookies.userData
+    if (!encryptedUserData) {
+        return res.status(401).send('Not authenticated')
+    }
 
-   try {
-      const decryptedData = JSON.parse(decrypt(token))
-      req.user = decryptedData
-      next()
-   } catch (err) {
-      return res.status(401).send('Not authenticated')
-   }
+    try {
+        const decryptedData = JSON.parse(decrypt(encryptedUserData))
+        
+        if (req.session.userId !== decryptedData.userId) {
+            return res.status(401).send('Session mismatch')
+        }
+        
+        req.user = decryptedData
+        next()
+    } catch (err) {
+        return res.status(401).send('Not authenticated')
+    }
 }
