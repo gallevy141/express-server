bb18f9b5c8346aeeda1d3c78ab6a7ef282681d03
Gal Levy <119522102+gallevy141@users.noreply.github.com>
2023-09-11 06:44:54 +0100

    create middleware func to validate and decrypt user data from the cookie

---
+++ myServer/routes/authMiddleware.js
@@ -0,0 +1,14 @@
+const { decrypt } = require('./cryptoUtil')
+
+module.exports = (req, res, next) => {
+   const token = req.cookies.userData
+   if (!token) return res.status(401).send('Not authenticated')
+
+   try {
+      const decryptedData = JSON.parse(decrypt(token))
+      req.user = decryptedData
+      next()
+   } catch (err) {
+      return res.status(401).send('Not authenticated')
+   }
+}
